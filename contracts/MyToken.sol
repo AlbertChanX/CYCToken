630e5f03018437ee8f245d17fd96d0d45532df14
apple <apple@apples-iMac.lan>
2018-05-05 18:09:30 +0800

    batchTransfer

---
+++ contracts/MyToken.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.21;
+
+import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+ 
+contract MyToken is StandardToken {
+
+    string public name;
+    string public symbol;
+    uint public decimals;
+
+
+//  uint256 public INITIAL_SUPPLY;
+//  21000000 * (10 ** uint256(decimals));
+
+  /**
+   * @dev Constructor that gives msg.sender all of existing tokens.
+   */
+  constructor(string NAME, string SYMBOL, uint DECIMALS, uint INITIAL_SUPPLY) public {
+    name = NAME;
+    symbol = SYMBOL;
+    decimals = DECIMALS;
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    emit Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+  }
+  // batch transfer ? to be audited
+  function batchTransfer(address[] _tos, uint256[] _amount) public returns (bool) {
+    require(_tos.length == _amount.length);
+    uint i;
+    for(i = 0; i < _tos.length; i++) {
+            transfer(_tos[i], _amount[i]);
+      emit Transfer(msg.sender, _tos[i], _amount[i]);
+    }
+    return true;
+  }
+}
--- migrations/2_deploy_contract..js
+++ migrations/2_deploy_contract..js
@@ -1,11 +1,11 @@
 // https://github.com/halls7588/ERC20-Token-and-ICO-Contract/blob/master/Lavevel-Token.sol
 // http://truffleframework.com/docs/getting_started/migrations
 // CYCToken/ .sol
-var CYCToken = artifacts.require("CYCToken");
+var CYCToken = artifacts.require("MyToken");
 
 module.exports = function(deployer) {
   // let initialSupply = 21000000e18 // 2100,0000
-  deployer.deploy(CYCToken);
+  deployer.deploy(MyToken, 'c', 'cc', 8, 100000);
 };
 
 // 0x32a26173c5f0d312938039ad8660f935e0328129
