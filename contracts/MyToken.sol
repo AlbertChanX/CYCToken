7e1f3eede9d9f8722715bad5f18a8e1140a90a98
apple <apple@apples-iMac.lan>
2018-05-28 16:39:41 +0800

    transferMultiple

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -8,29 +8,42 @@ contract MyToken is StandardToken {
     string public symbol;
     uint public decimals;
 
+      /**
+       * @dev Constructor that gives msg.sender all of existing tokens.
+       */
+      constructor(string NAME, string SYMBOL, uint DECIMALS, uint INITIAL_SUPPLY) public {
+        name = NAME;
+        symbol = SYMBOL;
+        decimals = DECIMALS;
+        totalSupply_ = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        emit Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+      }
 
-//  uint256 public INITIAL_SUPPLY;
-//  21000000 * (10 ** uint256(decimals));
+      // batch transfer ? to be audited
+//      function batchTransfer(address[] _tos, uint256[] _amount) public returns (bool) {
+//        require(_tos.length == _amount.length);
+//        for(uint256 i = 0; i < _tos.length; i++) {
+//                transfer(_tos[i], _amount[i]);
+//          emit Transfer(msg.sender, _tos[i], _amount[i]);
+//        }
+//        return true;
+//      }
 
-  /**
-   * @dev Constructor that gives msg.sender all of existing tokens.
-   */
-  constructor(string NAME, string SYMBOL, uint DECIMALS, uint INITIAL_SUPPLY) public {
-    name = NAME;
-    symbol = SYMBOL;
-    decimals = DECIMALS;
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-    emit Transfer(0x0, msg.sender, INITIAL_SUPPLY);
-  }
-  // batch transfer ? to be audited
-  function batchTransfer(address[] _tos, uint256[] _amount) public returns (bool) {
-    require(_tos.length == _amount.length);
-    uint i;
-    for(i = 0; i < _tos.length; i++) {
-            transfer(_tos[i], _amount[i]);
-      emit Transfer(msg.sender, _tos[i], _amount[i]);
+    function transferMultiple(address[] _tos, uint256[] _values)  public returns (bool) {
+        require(_tos.length == _values.length);
+        uint256 total = 0;
+
+        for(uint256 i = 0; i < _tos.length; i++) {
+            require(_tos[i] != address(0));
+            total = total.add(_values[i]);
+        }
+        require(total <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(total);
+        for(i = 0; i < _tos.length; i++) {
+            balances[_tos[i]] = balances[_tos[i]].add(_values[i]);
+            emit Transfer(msg.sender, _tos[i], _values[i]);
+        }
+        return true;
     }
-    return true;
-  }
 }
--- hdwallet.md
+++ hdwallet.md
@@ -9,6 +9,7 @@ npm install eth-lightwallet
 ## example
 in `node_modules/eth-lightwallet/example/webwallet.html`
 
+[ethereumjs/ethereumjs-wallet: Utilities for handling Ethereum keys](https://github.com/ethereumjs/ethereumjs-wallet)
 
 ## Refer
 1.[ConsenSys/eth-lightwallet](https://github.com/ConsenSys/eth-lightwallet/tree/b7b2d9656326de864542c521052df5fcc119d0f7)
@@ -23,6 +24,6 @@ in `node_modules/eth-lightwallet/example/webwallet.html`
 
 6.[`ropsten test coin` Ethernet Faucet](http://faucet.ropsten.be:3001/)
 
-7. [cyh5kf/ionic-wallet ](https://github.com/cyh5kf/ionic-wallet/tree/c4186c3e6d03217990d8182305abf475a761e8fa)
+7.[cyh5kf/ionic-wallet](https://github.com/cyh5kf/ionic-wallet/tree/c4186c3e6d03217990d8182305abf475a761e8fa)
 
-8. [isLishude/bitcoin-util ](https://github.com/isLishude/bitcoin-util/tree/a5ad3009bf28359f39c48c2c51768bef2787560e)
+8.[isLishude/bitcoin-util](https://github.com/isLishude/bitcoin-util/tree/a5ad3009bf28359f39c48c2c51768bef2787560e)
